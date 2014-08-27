b8bd36d8ae7d01830ee1e9e0cf5fe0a767d224e1
lzhangwei <zhangwei_999@foxmail.com>
2014-08-27 10:33:30 +0800

    update cart amounts

--- app/scripts/controllers/controller.js
+++ app/scripts/controllers/controller.js
@@ -5,7 +5,9 @@
 
 angular.module('angularLetusgoApp')
     .controller('IndexController', function ($scope, localStorageService) {
-        localStorageService.set('amounts', 0);
+        if(localStorageService.get('amounts') === undefined ){
+            localStorageService.set('amounts', 0);
+        }
         $scope.amounts = localStorageService.get('amounts');
 
         $scope.$on('to-parent-changeamounts', function() {
---
+++ test/spec/controllers/controller.js
@@ -0,0 +1,22 @@
+'use strict';
+
+describe('Controller: IndexController', function () {
+
+    // load the controller's module
+    beforeEach(module('angularLetusgoApp'));
+
+    var IndexController,
+        scope;
+
+    // Initialize the controller and a mock scope
+    beforeEach(inject(function ($controller, $rootScope) {
+        scope = $rootScope.$new();
+        IndexController = $controller('IndexController', {
+            $scope: scope
+        });
+    }));
+
+    it('should attach amounts to the scope', function () {
+        expect(scope.amounts !== undefined).toBe(true);
+    });
+});
