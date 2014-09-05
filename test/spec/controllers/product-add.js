2370b0585dd6e78880855aa3e1c16bb7b2072032
lzhangwei <zhangwei_999@foxmail.com>
2014-09-05 21:34:46 +0800

    add product add and update test

---
+++ test/spec/controllers/product-add.js
@@ -0,0 +1,37 @@
+/**
+ * Created by zhangwei on 14-9-5.
+ */
+'use strict';
+
+describe('Controller: ProductCtrl', function () {
+
+  var $scope, productService, createController, productList;
+
+  beforeEach(function () {
+    module('angularLetusgoApp');
+
+    inject(function ($injector) {
+
+      $scope = $injector.get('$rootScope').$new();
+      productService = $injector.get('productService');
+
+      var $controller = $injector.get('$controller');
+
+      createController = function () {
+        return $controller('ProductAddCtrl', {
+          $scope: $scope,
+          productService: productService
+        });
+      };
+    });
+
+    productList = [
+      {'barcode': 'ITEM000000', 'name': '可口可乐', 'unit': '瓶', 'price': 3.00, 'category': '饮料'},
+      {'barcode': 'ITEM000001', 'name': '雪碧', 'unit': '瓶', 'price': 3.00, 'category': '饮料'},
+      {'barcode': 'ITEM000002', 'name': '苹果', 'unit': '斤', 'price': 5.50, 'category': '水果'}
+    ];
+
+  });
+
+
+});
