9fff3670d7ccab44daeea3f12b2c879d23ff9bee
lzhangwei <zhangwei_999@foxmail.com>
2014-09-11 22:40:54 +0800

    update delete category service when have goods with this category

--- app/scripts/services/category-service.js
+++ app/scripts/services/category-service.js
@@ -1,7 +1,7 @@
 'use strict';
 (function (_) {
   angular.module('angularLetusgoApp')
-    .service('categoryService', function (localStorageService) {
+    .service('categoryService', function (localStorageService, productService) {
 
       this.getAllCategoryInfo = function () {
         return localStorageService.get('categorys');
@@ -26,15 +26,30 @@
 
       this.removeCategoryInfo = function (categoryInfo) {
         var categoryList = localStorageService.get('categorys');
-        var num = 0;
-        _.forEach(categoryList, function (item, index) {
-          if (item.id === categoryInfo.id) {
-            num = index;
+        if(isRemove(categoryInfo)){
+          var num = 0;
+          _.forEach(categoryList, function (item, index) {
+            if (item.id === categoryInfo.id) {
+              num = index;
+            }
+          });
+          categoryList.splice(num, 1);
+          localStorageService.set('categorys', categoryList);
+        }else{
+          
+        }
+        return categoryList;
+      };
+
+      var isRemove = function(categoryInfo) {
+        var productList = productService.getAllProductInfo();
+        var result = true;
+        _.forEach(productList, function(item){
+          if(result === true && item.category === categoryInfo.name){
+            result = false;
           }
         });
-        categoryList.splice(num, 1);
-        localStorageService.set('categorys', categoryList);
-        return categoryList;
+        return result;
       };
 
       this.updateCategoryInfo = function (categoryInfo) {
