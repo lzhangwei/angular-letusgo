3b1cacf6739aef4910885d943a51cdab323d6747
lzhangwei <zhangwei_999@foxmail.com>
2014-09-11 17:05:54 +0800

    add filter test

--- test/spec/filters/paging.js
+++ test/spec/filters/paging.js
@@ -5,8 +5,17 @@
 
 describe('Filter: pageitems', function () {
 
-  it('should filter the items', function () {
+  var items;
 
+  beforeEach(function () {
+    module('angularLetusgoApp');
+    items = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  });
+
+  it('should filter the items', function () {
+    inject(function(pageitemsFilter) {
+      expect(pageitemsFilter(items, 5)).toEqual([6, 7, 8, 9]);
+    });
   });
 
 });
