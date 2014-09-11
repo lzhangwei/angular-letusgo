db5e6e02646fc3d011c8d514e08401104d6e478a
lzhangwei <zhangwei_999@foxmail.com>
2014-09-11 10:23:08 +0800

    add paging in product manage

--- app/scripts/controllers/product.js
+++ app/scripts/controllers/product.js
@@ -12,6 +12,50 @@ angular.module('angularLetusgoApp')
     $scope.products = productService.getAllProductInfo();
 
 
+    $scope.itemsPerPage = 5;
+    $scope.currentPage = 1;
+
+    $scope.pageCount = function () {
+      var maxPage = Math.ceil($scope.products.length / $scope.itemsPerPage);
+      return maxPage;
+    };
+
+    $scope.range = function () {
+      var pages = [];
+      var i = 1;
+      var maxPage = $scope.pageCount();
+      while (i <= maxPage) {
+        pages.push(i);
+        i++;
+      }
+      return pages;
+    };
+
+    $scope.prevPage = function () {
+      if ($scope.currentPage > 1) {
+        $scope.currentPage--;
+      }
+    };
+
+    $scope.prevPageDisabled = function () {
+      return $scope.currentPage === 1 ? true : false;
+    };
+
+    $scope.nextPage = function () {
+      if ($scope.currentPage < $scope.pageCount()) {
+        $scope.currentPage++;
+      }
+    };
+
+    $scope.nextPageDisabled = function () {
+      return $scope.currentPage === $scope.pageCount() ? true : false;
+    };
+
+    $scope.setPage = function (n) {
+      $scope.currentPage = n;
+    };
+
+
     $scope.removeProductInfo = function (productInfo) {
       $scope.products = productService.removeProductInfo(productInfo);
     };
