1850a7fc9510514fbdd28885861b315e5ab2c896
lzhangwei <zhangwei_999@foxmail.com>
2014-09-13 18:12:53 +0800

    make update data in a function

--- app/scripts/controllers/cart.js
+++ app/scripts/controllers/cart.js
@@ -3,32 +3,37 @@
 angular.module('angularLetusgoApp')
   .controller('CartCtrl', function ($scope, cartService) {
 
-    $scope.$emit('to-parent-incart');
+    function emit(name) {
+      $scope.$emit(name);
+    }
+
+    function updateData(cartItemList){
+      $scope.cartItemGroup = cartService.categoryCartItem(cartItemList);
+      $scope.total = cartService.totalPrice(cartItemList);
+    }
+
+    emit('to-parent-incart');
 
     var cartItemList = cartService.getCartItem();
 
-    $scope.cartItemGroup = cartService.categoryCartItem(cartItemList);
-
-    $scope.total = cartService.totalPrice(cartItemList);
+    updateData(cartItemList);
 
     $scope.addCartItemClick = function (cartItem) {
 
       cartItemList = cartService.addCartItem(cartItem.item);
 
-      $scope.cartItemGroup = cartService.categoryCartItem(cartItemList);
-      $scope.total = cartService.totalPrice(cartItemList);
+      updateData(cartItemList);
 
-      $scope.$emit('to-parent-changeamounts');
+      emit('to-parent-changeamounts');
     };
 
     $scope.reduceCartItemClick = function (cartItem) {
 
       cartItemList = cartService.reduceCartItem(cartItem.item);
 
-      $scope.cartItemGroup = cartService.categoryCartItem(cartItemList);
-      $scope.total = cartService.totalPrice(cartItemList);
+      updateData(cartItemList);
 
-      $scope.$emit('to-parent-changeamounts');
+      emit('to-parent-changeamounts');
     };
 
     $scope.isShow = function () {
