cc0e8747be5b3936bc3e59512a1920fef072fc0e
lzhangwei <zhangwei_999@foxmail.com>
2014-09-13 13:07:20 +0800

    update product add

--- app/scripts/controllers/product-add.js
+++ app/scripts/controllers/product-add.js
@@ -14,11 +14,17 @@ angular.module('angularLetusgoApp')
     $scope.products = productService.getAllProductInfo();
 
     $scope.addProductInfo = function () {
-      var id = +$scope.products[$scope.products.length - 1].barcode.substring(9);
-      $scope.addproduct.barcode = $scope.products[$scope.products.length - 1].barcode.substring(0, 9) + (id + 1);
-      $scope.products = productService.addProductInfo($scope.addproduct);
-      $scope.addproduct = {};
-      $location.path('/product');
+      var isEmpty = $scope.addproduct.name && $scope.addproduct.category && $scope.addproduct.price && $scope.addproduct.unit;
+      if(isEmpty){
+        var id = +$scope.products[$scope.products.length - 1].barcode.substring(9);
+        $scope.addproduct.barcode = $scope.products[$scope.products.length - 1].barcode.substring(0, 9) + (id + 1);
+        $scope.products = productService.addProductInfo($scope.addproduct);
+        $scope.addproduct = {};
+        $location.path('/product');
+      }else{
+        alert('输入不能为空！');
+      }
+
     };
 
   });
